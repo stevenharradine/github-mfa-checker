c210ba69691adef3fb929bee5c8d2c978a669d2f
steven harradine <steven.harradine@telus.com>
2015-09-14 11:09:25 -0400

    works but shows userlist teams.length times

--- github-mfa-checker.js
+++ github-mfa-checker.js
@@ -8,71 +8,80 @@ var CONFIG  = require("./config"),
     },
     notifyList = []
 
-getAllUsersWithMfaDisabled (function (teams, mfa_disabled) {
-  traverseGroups (0, teams, mfa_disabled, function () {
-    showNotifyList ()
+getUsersWithMfaDisabled (function (mfa_disabled_users) {
+  getAdminTeamsInOrganization (function (teams) {
+    for (team_index in teams) {
+      var team = teams[team_index]
+
+      getMembersInTeam (mfa_disabled_users, team, function (members) {
+        for (member_index in members) {
+          var member = members[member_index],
+              login  = member.login
+
+          if (isUserOnNonMfaList (mfa_disabled_users, login)) {
+            addUserToNotifyList (login)
+          }
+
+          console.log ("m" + member_index + " " + (members.length - 1))
+          console.log ("t" + team_index + " " + (teams.length - 1))
+          console.log ("+" + (member_index == members.length - 1 && team_index == teams.length - 1))
+
+          if (member_index == members.length - 1 && team_index == teams.length - 1) {
+            showNotifyList ()
+          }
+        }
+      })
+    }
   })
 })
 
-function getAllUsersWithMfaDisabled (callback) {
+function getUsersWithMfaDisabled (callback) {
   curl("https://api.github.com/orgs/" + CONFIG.GITHUB_ORG + "/members?filter=2fa_disabled", options, function(err) {
-    var mfa_disabled = JSON.parse(this.body)
+    var mfa_disabled_users = JSON.parse(this.body)
 
     console.log ("Non-MFA enabled users in org:")
-    for (i in mfa_disabled) {
-      console.log ("\t" + mfa_disabled[i].login)
+    for (i in mfa_disabled_users) {
+      console.log ("\t" + mfa_disabled_users[i].login)
     }
 
-    curl("https://api.github.com/user/teams", options, function(err) {
-      var teams = JSON.parse(this.body)
-
-      callback (teams, mfa_disabled)
-    })
+    callback (mfa_disabled_users)
   })
 }
 
-function traverseGroups (team_index, teams, mfa_disabled, callback) {
-  if (teams[team_index].permission == "admin") {
-    var members_url = teams[team_index].members_url.replace ("{/member}", "")
+function getAdminTeamsInOrganization (callback) {
+  curl("https://api.github.com/orgs/" + CONFIG.GITHUB_ORG + "/teams", options, function(err) {
+    var teams = JSON.parse(this.body),
+        admin_teams = new Array ()
 
-    console.log (teams[team_index].name + ": " + members_url)
-    curl (members_url, options, function(err) {
-      if (err) console.log (err)
+    console.log ("Admin teams in organization:")
+    for (i in teams) {
+      if (teams[i].permission == "admin") {
+        console.log ("\t" + teams[i].name)
 
-      var admin_users = JSON.parse(this.body)
-
-      for (i in admin_users) {
-        var flagUser = userOnNonMfaList (mfa_disabled, admin_users[i].login)
-
-        console.log ("\t" + (flagUser ? "*" : "") + admin_users[i].login)
-
-        if (flagUser) {
-          addUserToNotifyList (admin_users[i].login)
-        }
+        admin_teams.push (teams[i])
       }
+    }
 
-      nextGroup (team_index, teams, mfa_disabled, function () {
-        callback()
-      })
-    })
-  } else {
-    nextGroup (team_index, teams, mfa_disabled, callback)
-  }
+    callback (admin_teams)
+  })
 }
 
-function nextGroup (team_index, teams, mfa_disabled, callback) {
-  var delay = 0
+function getMembersInTeam (mfa_disabled_users, team, callback) {
+  var this_curl = curl.create()
 
-  if (team_index++ < teams.length - 1) {
-    setTimeout(function(){
-      traverseGroups (team_index, teams, mfa_disabled, callback)
-    }, delay)
-  } else {
-    callback ()
-  }
+  this_curl(team.members_url.replace ("{/member}", ""), options, function(err) {
+    var members = JSON.parse(this.body)
+
+    console.log (team.name)
+    for (i in members) {
+      console.log ("\t" + (isUserOnNonMfaList (mfa_disabled_users, members[i].login) ? "*" : "")+ members[i].login)
+    }
+
+    callback (members)
+  })
 }
 
-function userOnNonMfaList (mfa_disabled, user) {
+function isUserOnNonMfaList (mfa_disabled, user) {
   for (index in mfa_disabled) {
     if (mfa_disabled[index].login == user) {
       return true
