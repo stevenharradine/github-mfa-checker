9e47d585e8d9545547dc9e7818bc4c8f101f7277
Steven Harradine <stevenharradine@gmail.com>
2016-03-03 16:41:28 -0500

    remove depencancy on node-curl which is no longer working, using native http

--- config.js
+++ config.js
@@ -1,2 +1,5 @@
-module.exports.GITHUB_TOKEN = "token"
-module.exports.GITHUB_ORG = "org-name"
+module.exports.GITHUB_TOKEN = ""
+module.exports.GITHUB_ORG = ""
+module.exports.SLACK_TOKEN = ""
+module.exports.SLACK_CHANNEL = ""
+module.exports.SLACK_LOG = ""
--- github-mfa-checker.js
+++ github-mfa-checker.js
@@ -1,60 +1,150 @@
 var CONFIG  = require("./config"),
-    curl    = require('node-curl'),
+    http = require("https"),
+    http2 = require("https"),
+    http3 = require("https"),
+    Slack = require('node-slack'),
+    notifyList = [],
     options = {
-      HTTPHEADER: [
-                   'User-Agent: github-mfa-checker',
-                   'Authorization: token ' + CONFIG.GITHUB_TOKEN
-                  ]
+      host: 'api.github.com',
+      port: 443,
+      path: "/orgs/" + CONFIG.GITHUB_ORG + "/members?filter=2fa_disabled",
+      method: 'GET',
+      headers: {
+        'User-Agent': 'github-mfa-checker',
+        'Authorization': 'token ' + CONFIG.GITHUB_TOKEN
+      }
     },
-    notifyList = []
+    buffered_out = ""
 
 getAllUsersWithMfaDisabled (function (teams, mfa_disabled) {
   traverseGroups (0, teams, mfa_disabled, function () {
-    showNotifyList ()
+    showNotifyList (function (message) {
+    var slack = new Slack("https://hooks.slack.com/services/" + CONFIG.SLACK_TOKEN)
+      if (notifyList.length > 0) {
+        slack.send({
+          text: message,
+          channel: CONFIG.SLACK_CHANNEL,
+          username: 'Github MFA Checker'
+        })
+      }
+
+      slack.send({
+        text: message,
+        channel: CONFIG.SLACK_LOG,
+        username: 'Github MFA Checker'
+      });
+    })
   })
 })
 
 function getAllUsersWithMfaDisabled (callback) {
-  curl("https://api.github.com/orgs/" + CONFIG.GITHUB_ORG + "/members?filter=2fa_disabled", options, function(err) {
-    var mfa_disabled = JSON.parse(this.body)
-
-    console.log ("Non-MFA enabled users in org:")
-    for (i in mfa_disabled) {
-      console.log ("\t" + mfa_disabled[i].login)
-    }
-
-    curl("https://api.github.com/user/teams", options, function(err) {
-      var teams = JSON.parse(this.body)
-
-      callback (teams, mfa_disabled)
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+    res.on('data', function (chunk) {
+      buffered_out+=chunk
     })
-  })
+    res.on('end', function () {
+      mfa_disabled = JSON.parse(buffered_out)
+      console.log ("Non-MFA enabled users in org:")
+      for (i in mfa_disabled) {
+        console.log ("\t" + mfa_disabled[i].login)
+      }
+
+      options = {
+        host: 'api.github.com',
+        port: 443,
+        path: "/user/teams",
+        method: 'GET',
+        headers: {
+          'User-Agent': 'github-mfa-checker',
+          'Authorization': 'token ' + CONFIG.GITHUB_TOKEN
+        }
+      }
+
+      var req2 = http2.request(options, function(res2) {
+        buffered_out = ""
+
+        res2.setEncoding('utf8');
+        res2.on('data', function (chunk) {
+          buffered_out+=chunk
+        })
+
+        res2.on('end', function () {
+          var teams_json = JSON.parse(buffered_out)
+
+          callback (teams_json, mfa_disabled)
+        })
+
+        res2.on('error', function(e) {
+          console.log('problem with request: ' + e.message);
+        });
+      })
+
+      req2.write('{}');
+      req2.end();
+    })
+  });
+
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+
+  // write data to request body
+  // req.write('');
+  req.write('{}');
+  req.end();
 }
 
 function traverseGroups (team_index, teams, mfa_disabled, callback) {
   if (teams[team_index].permission == "admin") {
     var members_url = teams[team_index].members_url.replace ("{/member}", "")
 
-    console.log (teams[team_index].name + ": " + members_url)
-    curl (members_url, options, function(err) {
-      if (err) console.log (err)
-
-      var admin_users = JSON.parse(this.body)
-
-      for (i in admin_users) {
-        var flagUser = userOnNonMfaList (mfa_disabled, admin_users[i].login)
-
-        console.log ("\t" + (flagUser ? "*" : "") + admin_users[i].login)
-
-        if (flagUser) {
-          addUserToNotifyList (admin_users[i].login)
-        }
+    options = {
+      host: 'api.github.com',
+      port: 443,
+      path: members_url,
+      method: 'GET',
+      headers: {
+        'User-Agent': 'github-mfa-checker',
+        'Authorization': 'token ' + CONFIG.GITHUB_TOKEN
       }
+    }
 
-      nextGroup (team_index, teams, mfa_disabled, function () {
-        callback()
+    console.log (teams[team_index].name + ": " + members_url)
+
+    var req3 = http3.request(options, function(res3) {
+      buffered_out = ""
+
+      res3.setEncoding('utf8');
+      res3.on('data', function (chunk) {
+        buffered_out+=chunk
       })
+
+      res3.on('end', function () {
+        var admin_users = JSON.parse(buffered_out)
+
+        for (i in admin_users) {
+          var flagUser = userOnNonMfaList (mfa_disabled, admin_users[i].login)
+
+          console.log ("\t" + (flagUser ? "*" : "") + admin_users[i].login)
+
+          if (flagUser) {
+            addUserToNotifyList (admin_users[i].login)
+          }
+        }
+
+        nextGroup (team_index, teams, mfa_disabled, function () {
+          callback()
+        })
+      })
+
+      res3.on('error', function(e) {
+        console.log('problem with request: ' + e.message);
+      });
     })
+
+    req3.write('{}');
+    req3.end();
   } else {
     nextGroup (team_index, teams, mfa_disabled, callback)
   }
@@ -96,9 +186,19 @@ function userNotOnNotifyList (user) {
   return true
 }
 
-function showNotifyList () {
-  console.log ("Admin users with MFA disabled:")
-  for (notifyList_index in notifyList) {
-    console.log ("\t" + notifyList[notifyList_index])
+function showNotifyList (callback) {
+  var buffered_out = ""
+
+  if (notifyList.length > 0) {
+    buffered_out += "Admin users with MFA disabled:"
+    for (notifyList_index in notifyList) {
+      buffered_out += "\n\t" + notifyList[notifyList_index]
+    }
+  } else {
+    buffered_out = "All admin users have mfa enabled"
   }
+
+  console.log (buffered_out)
+
+  callback (buffered_out)
 }
